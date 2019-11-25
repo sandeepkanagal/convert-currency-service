7e61a769d1351496610bb965a8fd5f52886560cb
Sandeep Kanagal <sandeep.kanagal@in.ibm.com>
2019-11-25 13:05:03 +0530

    LocalRibbonClientConfiguration class added

--- convert-currency-service/src/main/java/com/ibm/microservices/convertcurrencyservice/ConvertCurrencyServiceApplication.java
+++ convert-currency-service/src/main/java/com/ibm/microservices/convertcurrencyservice/ConvertCurrencyServiceApplication.java
@@ -3,11 +3,13 @@ package com.ibm.microservices.convertcurrencyservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.ibm.microservices.convertcurrencyservice")
 @EnableDiscoveryClient
+@RibbonClient(name = "convert-currency-service", configuration = LocalRibbonClientConfiguration.class)
 public class ConvertCurrencyServiceApplication {
 
 	public static void main(String[] args) {
